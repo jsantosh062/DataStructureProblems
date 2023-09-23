a9a2a136a475d94b69339b61c329e7b1b79a4d59
Kumar <sakumar97@TAGLDP27139.local>
2023-09-23 19:43:10 +0530

    Added Problem OnlyCommonCharacterStringFromAnArrayOfString

--- index.js
+++ index.js
@@ -1,19 +1,24 @@
-const { returnOnlyUniqueUsingJavascriptPrdefined, returnOnlyUniqueWithoutUsingJavascriptPrdefined } =  require('./returnOnlyUniqueValueFromAnArray')
-const { removeDuplicateFromAnArray,removeDuplicateUsingJavascriptPredefined } =  require('./removeDuplicateFromAnArray')
+const { returnOnlyUniqueUsingJavascriptPrdefined, returnOnlyUniqueWithoutUsingJavascriptPrdefined } = require('./returnOnlyUniqueValueFromAnArray')
+const { removeDuplicateFromAnArray, removeDuplicateUsingJavascriptPredefined } = require('./removeDuplicateFromAnArray')
+const { returnOnlyCommonCharacterStringFromAnArrayOfString } = require('./returnOnlyCommonCharacterStringFromAnArrayOfString')
 
 const arr = [1, 2, 4, 5, 6, 8, 8, 2, 2, 5, 4, 3, 4, 6, 9, 91, 31]
 
+const arr_1 = ["santosh", "ashok", "premjoshi", "gopikishan"]
+
 try {
 
-returnOnlyUniqueUsingJavascriptPrdefined(arr)
-  
-returnOnlyUniqueWithoutUsingJavascriptPrdefined(arr)
+  returnOnlyUniqueUsingJavascriptPrdefined(arr)
 
-removeDuplicateUsingJavascriptPredefined(arr)
-  
-removeDuplicateFromAnArray(arr)
+  returnOnlyUniqueWithoutUsingJavascriptPrdefined(arr)
 
-}catch(err) {
-  console.log("error =====>",err)
+  removeDuplicateUsingJavascriptPredefined(arr)
+
+  removeDuplicateFromAnArray(arr)
+
+  returnOnlyCommonCharacterStringFromAnArrayOfString(arr_1)
+
+} catch (err) {
+  console.log("error =====>", err)
 }
 
